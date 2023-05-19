1515fd787250c237b837c34401850ed2d5906aa8
Max <imaksmula@gmail.com>
2023-05-18 22:42:19 -0500

    created filter of monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      monsters: [],
+      filter: ''
+    }
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(
+        () => { return { monsters: users} },
+        () => { console.log(this.state) },
+      ))
+  }
+
+  render() {
+    console.log('render')
+    return (
+      <div className='App'>
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => this.setState({ filter: event.target.value })}
+        />
+        { this.state.monsters
+          .filter(monster => monster.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+          .map(monster => {
+            return (
+              <div key={ monster.id }>
+                <h1>{ monster.name }</h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;

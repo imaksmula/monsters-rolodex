63ebe2e97211b29f32be68259e21b0c04d0d1bb9
Max <imaksmula@gmail.com>
2023-05-19 23:17:37 -0500

    refactored into components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Component } from 'react'
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component { 
@@ -7,13 +10,11 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      filter: ''
+      searchField: ''
     }
-    console.log('constructor')
   }
 
   componentDidMount() {
-    console.log('ComponentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(
@@ -22,26 +23,27 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
-    console.log('render')
+
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
     return (
       <div className='App'>
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={(event) => this.setState({ filter: event.target.value })}
+        <SearchBox 
+          className='search-box'
+          placeholder='search monsters'
+          onChangeHandler={onSearchChange}
         />
-        { this.state.monsters
-          .filter(monster => monster.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-          .map(monster => {
-            return (
-              <div key={ monster.id }>
-                <h1>{ monster.name }</h1>
-              </div>
-            )
-          })
-        }
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
